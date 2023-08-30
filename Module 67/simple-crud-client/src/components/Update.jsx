61ba7ad00593e36247db260ab9a7abb96f037063
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-08-30 19:26:59 +0600

    update

--- Module 67/simple-crud-client/src/components/Update.jsx
+++ Module 67/simple-crud-client/src/components/Update.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
-  const user = useLoaderData();
+  const userToUpdate = useLoaderData();
 
   const handleUpdate = (e) => {
     e.preventDefault();
@@ -10,20 +10,38 @@ const Update = () => {
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
-    const user = { name, email };
-    console.log(user);
+    const updatedUser = { name, email };
+
+    fetch(`http://localhost:5000/users/${userToUpdate._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        data.modifiedCount !== 0 && window.alert("user updated successfully");
+      });
   };
+
+  // console.log(userToUpdate);
 
   return (
     <div>
       <h4>
-        Update information of {user.name} - {user.email}
+        Update information of {userToUpdate.name} - {userToUpdate.email}
       </h4>
 
       <form action="" onSubmit={handleUpdate}>
-        <input type="text" name="name" defaultValue={user.name} id="" />
+        <input type="text" name="name" defaultValue={userToUpdate.name} id="" />
         <br />
-        <input type="email" name="email" defaultValue={user.email} id="" />
+        <input
+          type="email"
+          name="email"
+          defaultValue={userToUpdate.email}
+          id=""
+        />
         <br />
         <input type="submit" value="Update" />
       </form>
--- Module 67/simple-crud-server/index.js
+++ Module 67/simple-crud-server/index.js
@@ -53,6 +53,29 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email,
+        },
+      };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedUser,
+        options
+      );
+
+      res.send(result);
+    });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
 
